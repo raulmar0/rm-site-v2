a209b1984f21eedeb570028ae9799260f6e57fe4
raulmar <raulhdz40@gmail.com>
2023-04-24 22:55:44 -0600

    mockup migration to angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { BioComponent } from './bio/bio.component';
+import { NavComponent } from './nav/nav.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HeaderComponent,
+    BioComponent,
+    NavComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bio/bio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rm-bio',
+  templateUrl: './bio.component.html',
+  styleUrls: ['./bio.component.css']
+})
+export class BioComponent {
+
+}
