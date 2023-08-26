2893f9473ad5f66c94710159bfd607b09f3cfac0
raulmar <raulhdz40@gmail.com>
2023-08-25 23:42:57 -0600

    Adding scully and fixing its styling issues

---
+++ scully.rm-site-v2.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'rm-site-v2',
+  outDir: './dist/static',
+  routes: {},
+};
