bf7b763c9fe3a2972df874eee308cdd93f004b7b
raulmar <raulhdz40@gmail.com>
2024-01-27 14:07:38 -0600

    deleting ssr files to reinstall angular ssr

--- scully.rm-site-v2.config.ts
+++
@@ -1,8 +0,0 @@
-import { ScullyConfig } from '@scullyio/scully';
-
-export const config: ScullyConfig = {
-  projectRoot: './src',
-  projectName: 'rm-site-v2',
-  outDir: './dist/static',
-  routes: {},
-};
